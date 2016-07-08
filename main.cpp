8b45255836c0d44aafce745c46acd6d1761b3ed3
Ghabriel <ghabriel.nunes@hotmail.com>
2016-07-07 22:35:10 -0300

    Finished the implementation of loadObj and drawCharacter

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <GL/glut.h>
 #include <iostream>
+#include <sstream>
 #include <vector>
 
 #define TRACE(x) std::cout << (#x) << " = " << (x) << std::endl
@@ -30,18 +31,67 @@ GLuint texture;
 // -----------------------------------------
 
 void loadObj(const std::string& filename) {
-	std::ifstream input(filename);
-	std::string line;
-	while (std::getline(input, line)) {
-		if (line.size() == 0 || line[0] == '#') {
-			continue;
-		}
-		TRACE(line);
-	}
+    vertices.clear();
+    normals.clear();
+    faces.clear();
+    textures.clear();
+    std::ifstream input(filename);
+    std::string line;
+    while (std::getline(input, line)) {
+        if (line.size() == 0 || line[0] == '#') {
+            continue;
+        }
+        std::istringstream stream(line);
+        std::string command;
+        stream >> command;
+        if (command == "v") {
+            Point vertex;
+            stream >> vertex[0];
+            stream >> vertex[1];
+            stream >> vertex[2];
+            vertices.push_back(vertex);
+        } else if (command == "vn") {
+            Point vertex;
+            stream >> vertex[0];
+            stream >> vertex[1];
+            stream >> vertex[2];
+            normals.push_back(vertex);
+        } else if (command == "vt") {
+            Texture newTexture;
+            stream >> newTexture[0];
+            stream >> newTexture[1];
+            textures.push_back(newTexture);
+        } else if (command == "f") {
+            Triangle face;
+            for (unsigned i = 0; i < 3; i++) {
+                for (unsigned j = 0; j < 3; j++) {
+                    stream >> face[j][i];
+                }
+            }
+            faces.push_back(face);
+        }
+    }
 }
 
 void drawCharacter() {
-	ECHO("Drawing character...");
+    ECHO("Drawing character...");
+    glPushMatrix();
+    glScaled(3.0, 3.0, 3.0);
+
+    glBindTexture(GL_TEXTURE_2D, texture);
+    for (auto face : faces) {
+        glBegin(GL_TRIANGLES);
+        for (unsigned i = 0; i < 3; i++) {
+            auto vertex = vertices[face[0][i]];
+            auto currTexture = textures[face[1][i]];
+            auto normal = normals[face[2][i]];
+            glTexCoord2d(currTexture[0], currTexture[1]);
+            glVertex3d(vertex[0], vertex[1], vertex[2]);
+            glNormal3d(normal[0], normal[1], normal[2]);
+        }
+        glEnd();
+    }
+    glPopMatrix();
 }
 
 void reshape(int newWidth, int newHeight) {
@@ -76,6 +126,7 @@ bool init() {
     glDepthFunc(GL_LEQUAL);
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
     loadObj("subzero.obj");
+    drawCharacter();
     return true;
 }
 
@@ -90,16 +141,16 @@ void onSpecialKeyPress(int key, int mouseX, int mouseY) {
 }
 
 int main(int argc, char** argv) {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-	glutInitWindowSize(winWidth, winHeight);
-	glutInitWindowPosition(0, 0);
-	auto window = glutCreateWindow("YAHMA");
-	glutReshapeFunc(reshape);
-	glutDisplayFunc(display);
-	glutKeyboardFunc(onKeyPress);
-	glutSpecialFunc(onSpecialKeyPress);
-	glutIdleFunc(idle);
-	init();
-	glutMainLoop();
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowSize(winWidth, winHeight);
+    glutInitWindowPosition(0, 0);
+    auto window = glutCreateWindow("YAHMA");
+    glutReshapeFunc(reshape);
+    glutDisplayFunc(display);
+    glutKeyboardFunc(onKeyPress);
+    glutSpecialFunc(onSpecialKeyPress);
+    glutIdleFunc(idle);
+    init();
+    glutMainLoop();
 }
